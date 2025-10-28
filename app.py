9a2030d3ec1a361756911f4166012fc52a1dc2ff
AliAhmad552 <frextarr.552@gmail.com>
2025-10-28 16:48:57 +0500

    add imports in app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from tensorflow.keras.models import load_model
 import numpy as np
-import pandas as pd
+from PIL import Image
+import pickle
+import os
