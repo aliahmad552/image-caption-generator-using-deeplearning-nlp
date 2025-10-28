f39389994d4a0663b80f1dfa78fc44bcc052bf1c
AliAhmad552 <frextarr.552@gmail.com>
2025-10-28 17:04:01 +0500

    add generate caption function

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image
 import pickle
 import os
 
-app = Fastapi()
+app = FastAPI()
 
 # Load model, tokenizer, mapping, and features
 model = load_model("model_best1.h5")
@@ -28,3 +28,21 @@ def preprocess_image(image_path, target_size=(299, 299)):
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array / 255.0  # normalize if model expects
     return img_array
+
+# Generate caption function
+def generate_caption(model, image_feature, tokenizer, max_length=34):
+    in_text = "startseq"
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = np.pad(sequence, (0, max_length-len(sequence)), mode='constant')
+        yhat = model.predict([image_feature, np.array([sequence])], verbose=0)
+        yhat = np.argmax(yhat)
+        word = tokenizer.index_word.get(yhat, None)
+        if word is None:
+            break
+        in_text += " " + word
+        if word == "endseq":
+            break
+    final_caption = in_text.replace("startseq", "").replace("endseq", "").strip()
+    return final_caption
+
